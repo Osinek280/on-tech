e9dbb500a036f5c70133e01416c8f25741a7ec18
Szymon <szymon.osinski.2009@gmail.com>
2024-12-20 17:51:45 +0100

    DevTrack: Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
-export function add({a, b}: {a: number, b: number}) {
-  return a + b
+import Accessories from "@/components/accessories";
+import Comparison from "@/components/comparison";
+import ContactForm from "@/components/contactForm";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Hero from "@/components/hero";
+import PricingCalculator from "@/components/PricingCalculator";
+import { classicCaps, classicConnectors, softlineCaps, softlineConnectors } from "@/constants";
+
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <Hero />
+      <Comparison />
+      <Accessories title="Akcesoria dla Classic" connectors={classicConnectors} caps={classicCaps} />
+      <Accessories title="Akcesoria dla Classic" connectors={softlineConnectors} caps={softlineCaps} />
+      <PricingCalculator />
+      <ContactForm />
+      <Footer />
+    </div>
+  );
 }
