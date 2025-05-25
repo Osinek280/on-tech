8618a47b3f2983eb50d9c104b410b3197a5e719f
Szymon <szymon.osinski.2009@gmail.com>
2025-05-25 13:05:54 +0200

    return old next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,7 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-  images: {
-    unoptimized: true,
-    // remotePatterns: [
-    //   {
-    //     protocol: "https",
-    //     hostname: "cdn.on-tech.net.pl",
-    //     port: "",
-    //     pathname: "/**",
-    //   },
-    // ],
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
